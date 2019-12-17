aa8be4f683aec66e8be54cdcb22dbf236e0d5b9e
JorgenBy <jorgenbygdemark@gmail.com>
2019-12-17 23:18:14 +0100

    updates

---
+++ TraderaWebServiceClient/C_CategoryItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraderaWebServiceClient
+{
+    class C_CategoryItem
+    {
+        private string name;
+        private int id;
+        private int[] subcategories;
+
+        public C_CategoryItem(string name, int id)
+        {
+            this.name = name;
+            this.id = id;
+        }
+
+        public void AddSubCategory(int subId)
+        {
+            this.subcategories.Append(subId);
+        }
+    }
+}
--- TraderaWebServiceClient/TraderaPublicService.cs
+++ TraderaWebServiceClient/TraderaPublicService.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
 
 namespace TraderaWebServiceClient
 {
@@ -30,21 +34,63 @@ namespace TraderaWebServiceClient
         // TODO alla subkategorier borde går att hämta genom att gå neråt i hierakin....
         public void GetCategories()
         {
+            ArrayList categoryList = new ArrayList();
             Tradera.Category[] categories = this.publicService.GetCategories();
-
+            
             Console.WriteLine(categories.Length);
 
-            for(int i = 0; i < categories.Length; i++)
+            XmlReader rdr = XmlReader.Create(new System.IO.StringReader(ToXML(categories)));
+            while (rdr.Read())
             {
-                Tradera.Category category = categories[i];
-
-                Console.WriteLine(category.Name);
-                Tradera.Category[] cat = category.Category1;
-                Tradera.Category cat2 = cat[0];
-                Console.WriteLine(cat2.Name);
+            
+                if (rdr.NodeType == XmlNodeType.Element)
+                {
+                    Console.WriteLine(rdr.LocalName);
+                    if (rdr.HasAttributes)
+                    {
+                        int id = ToInt(rdr.GetAttribute("Id"));
+                        string name = rdr.GetAttribute("Name");
+                        Console.WriteLine(rdr.GetAttribute("Id"));
+                        Console.WriteLine(rdr.GetAttribute("Name"));
+                        C_CategoryItem newItem = new C_CategoryItem(name, id);
+                        categoryList.Add(newItem);
+                    }
+                }
             }
         }
 
+        public string ToXML(object convertItem)
+        {
+            using (var stringwriter = new System.IO.StringWriter())
+            {
+                var serializer = new XmlSerializer(convertItem.GetType());
+                serializer.Serialize(stringwriter, convertItem);
+                return stringwriter.ToString();
+            }
+        }
+
+
+        public int ToInt(string intString)
+        {
+            int number = 0;
+            try
+            {
+                 number = Convert.ToInt32(intString);
+            }
+            catch (FormatException)
+            {
+                // the FormatException is thrown when the string text does 
+                // not represent a valid integer.
+            }
+            catch (OverflowException)
+            {
+                // the OverflowException is thrown when the string is a valid integer, 
+                // but is too large for a 32 bit integer.  Use Convert.ToInt64 in
+                // this case.
+            }
+
+            return number;
+        }
 
     }
 }
